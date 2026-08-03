dedee9a318e4a3c4ca4626a3c7f698b07e85e93c
Avinash Kumar Deepak <avinash8655279@gmail.com>
2026-08-03 09:58:26 +0530

    fix(workloadmanager): bind child deletes to observed objects
    Signed-off-by: Avinash Kumar Deepak <avinash8655279@gmail.com>

--- pkg/workloadmanager/codeinterpreter_controller.go
+++ pkg/workloadmanager/codeinterpreter_controller.go
@@ -303,7 +303,10 @@ func (r *CodeInterpreterReconciler) deleteSandboxWarmPool(ctx context.Context, c
 		return err
 	}
 
-	if err := r.Delete(ctx, warmPool); err != nil {
+	if err := r.Delete(ctx, warmPool, client.Preconditions{
+		UID:             &warmPool.UID,
+		ResourceVersion: &warmPool.ResourceVersion,
+	}); err != nil {
 		if !errors.IsNotFound(err) {
 			return fmt.Errorf("failed to delete SandboxWarmPool: %w", err)
 		}
@@ -326,7 +329,10 @@ func (r *CodeInterpreterReconciler) deleteSandboxTemplate(ctx context.Context, c
 		return err
 	}
 
-	if err := r.Delete(ctx, sandboxTemplate); err != nil {
+	if err := r.Delete(ctx, sandboxTemplate, client.Preconditions{
+		UID:             &sandboxTemplate.UID,
+		ResourceVersion: &sandboxTemplate.ResourceVersion,
+	}); err != nil {
 		if !errors.IsNotFound(err) {
 			return fmt.Errorf("failed to delete SandboxTemplate: %w", err)
 		}
